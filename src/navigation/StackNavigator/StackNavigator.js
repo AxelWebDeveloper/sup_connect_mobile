425dfefc2a3cc7ce122ad4b1b1f500e5df9b809a
alexismajchrzak <72200961+alexismajchrzak@users.noreply.github.com>
2023-04-27 13:58:41 +0200

    alexis-chat-front

--- src/navigation/StackNavigator/StackNavigator.js
+++ src/navigation/StackNavigator/StackNavigator.js
@@ -3,14 +3,16 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../../screens/Login/Login';
 import Home from '../../screens/Home/Home';
 import CompleteNewPassword from '../../screens/CompleteNewPassword/CompleteNewPassword';
+import Discussion from '../../screens/Discussion/Discussion';
 const Stack = createStackNavigator();
 
 function StackNavigator() {
   return (
     <Stack.Navigator
       screenOptions={{headerShown: false}}
-      initialRouteName={'Login'}>
+      initialRouteName={'Discussion'}>
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Discussion" component={Discussion} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen
         name="CompleteNewPassword"
